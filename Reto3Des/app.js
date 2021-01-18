f0950f2e45cf1e3953a30e09866ce32e67cfb0aa
InakiSagrario <inaki.sagrariopa@elorrieta-errekamari.com>
2021-01-18 18:49:55 +0100

    Servidor en node

---
+++ Reto3Des/app.js
@@ -0,0 +1,31 @@
+
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const routes = require('./routes/index.js');
+
+// Ajustes del servidor
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('puerto', process.env.PORT || 3000);
+
+// middlewares
+app.use((req, res, next) => {
+	console.log(`${req.url} - ${req.method}`);
+	next();
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Rutas
+app.use(routes);
+
+// satic files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// bootstraping the app
+app.listen(app.get('puerto'), () => {
+  console.log('server activo en el puerto', app.get('puerto'))
+});
